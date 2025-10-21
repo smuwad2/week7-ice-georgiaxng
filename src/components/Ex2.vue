d111f2738bd62976c142e4d71f0361ebac909012
georgiaxng <73217466+georgiaxng@users.noreply.github.com>
2025-10-21 01:57:48 +0000

    ex2 done

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,16 +3,32 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                itemInput:''
             }
             
-        }
+        },
+  methods: {
+    deleteItem(item) {
+      // remove the clicked item from the list
+      this.items = this.items.filter(i => i !== item);
+    },
+    addItem(){
+this.items.push(this.itemInput)
+    }
+  }
     }
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
-   
+    <div >
+    <li v-for="item in items" :key="item">
+      {{ item }}
+      <button @click="deleteItem(item)">Delete!</button>
+    </li>
+    </div>
+
+    <input v-model="itemInput"></input><button @click="addItem">Add!</button>
 </template>
